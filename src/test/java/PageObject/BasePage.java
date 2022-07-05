db8d1f3259c9e09c9713aacc894d25737e4e247a
Hleb <illlingleb@gmail.com>
2022-07-05 13:43:38 +0300

    AddInfoBasePageandTestInit

--- src/test/java/PageObject/BasePage.java
+++ src/test/java/PageObject/BasePage.java
@@ -1,4 +1,31 @@
 package PageObject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
 public class BasePage {
+    public WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement getElementByXpath(String locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        return driver.findElement(By.xpath(locator));
+    }
+
+    public List<WebElement> getElementsByXpath(String locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        return driver.findElements(By.xpath(locator));
+    }
 }
+
---
+++ src/test/java/Tests/TestInit.java
@@ -0,0 +1,46 @@
+package Tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestInit {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterMethod
+    public void finishSession() {
+        driver.quit();
+    }
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void fullScreen() {
+        driver.manage().window().maximize();
+    }
+
+
+    protected String getURL() {
+        return driver.getCurrentUrl();
+
+    }
+}
+
+
