7137414335db2d1dc0605b5e16b154732112270b
Hleb <illlingleb@gmail.com>
2022-07-07 13:15:15 +0300

    NewTestLoginMoyo

---
+++ src/test/java/Tests/Moyo/TestLoginMoyo.java
@@ -0,0 +1,18 @@
+package Tests.Moyo;
+
+import PageObject.Moyo.ua.MoyoHomePage;
+import Tests.TestInit;
+import org.testng.annotations.Test;
+
+public class TestLoginMoyo extends TestInit {
+
+    @Test
+    public void testLoginMoyo() {
+        MoyoHomePage moyoHomePage = new MoyoHomePage(driver);
+
+        goToSite("https://www.moyo.ua/");
+        moyoHomePage.getEnterBtn().click();
+        moyoHomePage.getSearchPhoneOrEmailBtn().sendKeys("illlingleb@gmail.com \n");
+        moyoHomePage.getDropBoxEnterBtn().click();
+    }
+}
