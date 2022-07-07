3efa6289a90a3c6a2474035f5ad117c8a75a4aa0
Max <chirvamaxym@gmail.com>
2022-07-07 11:33:41 +0300

    Add Rozetka Sign Up Test

---
+++ src/test/java/PageObject/RozetkaHomePageWebElements.java
@@ -0,0 +1,50 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class RozetkaHomePageWebElements extends BasePage {
+
+    public RozetkaHomePageWebElements(WebDriver driver) {
+        super(driver);
+    }
+
+    //HEADER
+    public WebElement getProfileBtn() {
+        return getElementByXpath("//li[@class='header-actions__item header-actions__item--user']");
+    }
+
+    // SIGN IN WINDOW
+    public WebElement getSignUpBtn() {
+        return getElementByXpath("//button[@class='auth-modal__register-link button button--link ng-star-inserted']");
+    }
+
+    // SIGN UP WINDOW
+    public WebElement getSignUpFirstNameField() {
+        return getElementByXpath("//input[@id='registerUserName']");
+    }
+
+    public WebElement getSignUpLastNameField() {
+        return getElementByXpath("//input[@id='registerUserSurname']");
+    }
+
+    public WebElement getSignUpNumberField() {
+        return getElementByXpath("//input[@id='registerUserPhone']");
+    }
+
+    public WebElement getSignUpEmailField() {
+        return getElementByXpath("//input[@id='registerUserEmail']");
+    }
+
+    public WebElement getSignUpPasswordField() {
+        return getElementByXpath("//input[@id='registerUserPassword']");
+    }
+
+    public WebElement getGoSignUpBtn() {
+        return getElementByXpath("//button[@class='button button--large button--green auth-modal__submit']");
+    }
+
+    public WebElement alertUserWithThisNumberAlreadyRegistered() {
+        return getElementByXpath("//p[@class='validation-message ng-star-inserted']");
+    }
+}
