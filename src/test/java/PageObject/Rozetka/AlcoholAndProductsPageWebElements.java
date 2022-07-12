710ef6f2e6bc0b35324dd529af80f7ea51057a5c
Max <chirvamaxym@gmail.com>
2022-07-12 15:41:56 +0300

    Test adding products to review category

---
+++ src/test/java/PageObject/Rozetka/AlcoholAndProductsPageWebElements.java
@@ -0,0 +1,34 @@
+package PageObject.Rozetka;
+
+import PageObject.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AlcoholAndProductsPageWebElements extends BasePage {
+
+    public AlcoholAndProductsPageWebElements(WebDriver driver) {
+        super(driver);
+    }
+
+    public WebElement confirmAgeBtn(){
+        return getElementByXpath("//div[@class='rz-banner__btn rz-btn_blue btn-link']");
+    }
+
+    //CATEGORIES
+
+    public WebElement getStrongDrinksBtn(){
+        return getElementByXpath("//a[@href='https://rozetka.com.ua/krepkie-napitki/c4594292/'][@class='tile-cats__heading ng-star-inserted']");
+    }
+
+    //STRONG DRINKS CATEGORIES
+
+    public WebElement getWhiskeyBtn(){
+        return getElementByXpath("//a[@href='https://rozetka.com.ua/ua/viski/c4649130/']");
+    }
+
+    //WHISKEY PRODUCTS
+
+    public WebElement getJamesonWhiskey1LBtn(){
+        return getElementByXpath("//div[@data-goods-id='4719078']");
+    }
+}
