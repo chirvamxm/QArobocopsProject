8dfd975417b8d45a09ea52ce2d2aa4ae4da9ccb8
Roman Dovhalenko <romandovhalenko@gmail.com>
2022-07-13 20:55:03 +0300

    QAR-103_AddTestCategoryKidsWorld

---
+++ src/test/java/PageObject/Moyo/ua/MoyoPageCategoryKidsWorld.java
@@ -0,0 +1,15 @@
+package PageObject.Moyo.ua;
+
+import PageObject.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MoyoPageCategoryKidsWorld extends BasePage {
+    public MoyoPageCategoryKidsWorld(WebDriver driver) {
+        super(driver);
+    }
+
+    public WebElement getTopGoodsFirst() {
+        return getElementsByXpath("//button[@class='buy-btn btn btn--green js-product-modal-close js-buy']").get(0);
+    }
+}
