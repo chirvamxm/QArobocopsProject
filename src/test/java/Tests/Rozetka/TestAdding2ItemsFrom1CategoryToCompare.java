b7d83ac265d48fcd4c577cf79f27d697f0cf9db3
Max <chirvamaxym@gmail.com>
2022-07-13 12:28:09 +0300

    QAR-68 Test Adding 2 items from 1 Category to compare

---
+++ src/test/java/Tests/Rozetka/TestAdding2ItemsFrom1CategoryToCompare.java
@@ -0,0 +1,38 @@
+package Tests.Rozetka;
+
+import PageObject.Rozetka.AlcoholAndProductsPageWebElements;
+import PageObject.Rozetka.HomePageWebElements;
+import PageObject.Rozetka.ItemsPageWebElements;
+import Tests.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestAdding2ItemsFrom1CategoryToCompare extends TestInit {
+
+    @Test
+    public void testAdding2ItemsFrom1CategoryToCompare() {
+        HomePageWebElements homePageWebElements = new HomePageWebElements(driver);
+        ItemsPageWebElements itemsPageWebElements = new ItemsPageWebElements(driver);
+        AlcoholAndProductsPageWebElements alcoholAndProductsPageWebElements = new AlcoholAndProductsPageWebElements(driver);
+
+        goToSite("https://rozetka.com.ua/ua/");
+        homePageWebElements.getCatalogueBtn().click();
+        homePageWebElements.getMenuCategoryAlcoholAndProducts().click();
+        alcoholAndProductsPageWebElements.getStrongDrinksBtn().click();
+        alcoholAndProductsPageWebElements.confirmAgeBtn().click();
+        alcoholAndProductsPageWebElements.getWhiskeyBtn().click();
+        alcoholAndProductsPageWebElements.getBallantineFines1LBtn().click();
+        sleep(1); //Wait for site loading
+        itemsPageWebElements.getAddToComparisonBtn().click();
+        homePageWebElements.getCatalogueBtn().click();
+        homePageWebElements.getMenuCategoryAlcoholAndProducts().click();
+        alcoholAndProductsPageWebElements.getStrongDrinksBtn().click();
+        alcoholAndProductsPageWebElements.getWhiskeyBtn().click();
+        alcoholAndProductsPageWebElements.getJamesonWhiskey1LBtn().click();
+        sleep(1); //Wait for site loading
+        itemsPageWebElements.getAddToComparisonBtn().click();
+        homePageWebElements.getComparisonBtn().click();
+        itemsPageWebElements.getToComparisonListBtn().click();
+        Assert.assertTrue(itemsPageWebElements.displayed2ItemsInCompare().isDisplayed());
+    }
+}
