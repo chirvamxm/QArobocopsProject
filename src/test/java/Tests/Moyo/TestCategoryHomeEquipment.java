0faae98e19fd7a1761b67e074c84129c6ec96369
Roman Dovhalenko <romandovhalenko@gmail.com>
2022-07-13 14:21:47 +0300

    QAR-91_AddTestCategoryHomeEquipment

---
+++ src/test/java/Tests/Moyo/TestCategoryHomeEquipment.java
@@ -0,0 +1,46 @@
+package Tests.Moyo;
+
+import PageObject.Moyo.ua.*;
+import Tests.TestInit;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class TestCategoryHomeEquipment extends TestInit {
+
+    @Test
+    public void testCategoryHomeEquipment() {
+        MoyoHomePage moyoHomePage = new MoyoHomePage(driver);
+        MoyoPageCategoryHomeEquipment moyoPageCategoryHomeEquipment = new MoyoPageCategoryHomeEquipment(driver);
+        MoyoPageBasket moyoPageBasket = new MoyoPageBasket(driver);
+        MoyoPageOrder moyoPageOrder = new MoyoPageOrder(driver);
+
+        goToSite("https://www.moyo.ua/ua/");
+        sleep(2);
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeEquipment().click();
+        sleep(2);
+        moyoPageCategoryHomeEquipment.getTopGoodsFirst().sendKeys(Keys.ENTER);
+        sleep(2);
+        moyoPageBasket.getBuyBtn().sendKeys(Keys.ENTER);
+        sleep(2);
+        moyoPageOrder.getBtnNewBuyer().click();
+        sleep(2);
+        moyoPageOrder.getFieldFirstName().sendKeys("Імя");
+        moyoPageOrder.getFieldMiddleName().sendKeys("По-батькові");
+        moyoPageOrder.getFieldLastname().sendKeys("Прізвище");
+        moyoPageOrder.getFieldPhone().sendKeys("671234567");
+        moyoPageOrder.getFieldEmail().sendKeys("test.671234567@gmail.com");
+        moyoPageOrder.getBtnNext().click();
+        sleep(2);
+        moyoPageOrder.getBtnMagazine().click();
+        moyoPageOrder.getBtnCity().click();
+        moyoPageOrder.getFieldCity().sendKeys("Суми\n");
+        sleep(2);
+        moyoPageOrder.getFieldCityFirstItem().click();
+        sleep(2);
+        moyoPageOrder.getBtnNext().click();
+        sleep(2);
+        moyoPageOrder.getBtnPaymentInMagazine().click();
+        moyoPageOrder.getBtnOrderCancel().click();
+    }
+}
