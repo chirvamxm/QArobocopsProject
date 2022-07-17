fc52bf69aad2e4be1fbae214b4b55269548464c9
Roman Dovhalenko <romandovhalenko@gmail.com>
2022-07-17 17:53:04 +0300

    QAR-125_AddTestSubMenuHomeGoods

--- src/test/java/PageObject/Moyo/ua/MoyoPageCategoryHomeGoods.java
+++ src/test/java/PageObject/Moyo/ua/MoyoPageCategoryHomeGoods.java
@@ -12,4 +12,36 @@ public class MoyoPageCategoryHomeGoods extends BasePage {
     public WebElement getTopGoodsFirst() {
         return getElementsByXpath("//button[@class='buy-btn btn btn--green js-product-modal-close js-buy']").get(0);
     }
+
+    public WebElement getSubCookware() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(0);
+    }
+
+    public WebElement getSubDishesForServing() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(1);
+    }
+
+    public WebElement getSubPlumbing() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(2);
+    }
+
+    public WebElement getSubLighting() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(3);
+    }
+
+    public WebElement getSubSecurityAndAlarm() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(4);
+    }
+
+    public WebElement getSubAlternativeEnergy() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(5);
+    }
+
+    public WebElement getSubHomeDecor() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(6);
+    }
+
+    public WebElement getSubBagsAndAccessories() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(7);
+    }
 }
---
+++ src/test/java/Tests/Moyo/TestSubMenuHomeGoods.java
@@ -0,0 +1,52 @@
+package Tests.Moyo;
+
+import PageObject.Moyo.ua.MoyoHomePage;
+import PageObject.Moyo.ua.MoyoPageCategoryHomeGoods;
+import Tests.TestInit;
+import org.testng.annotations.Test;
+
+public class TestSubMenuHomeGoods extends TestInit {
+
+    @Test
+    public void testSubMenuHomeGoods() {
+        MoyoHomePage moyoHomePage = new MoyoHomePage(driver);
+        MoyoPageCategoryHomeGoods moyoPageCategoryHomeGoods = new MoyoPageCategoryHomeGoods(driver);
+
+        goToSite("https://www.moyo.ua/ua/");
+        sleep(1);
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeGoods().click();
+        sleep(1);
+        moyoPageCategoryHomeGoods.getSubCookware().click();
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeGoods().click();
+        sleep(1);
+        moyoPageCategoryHomeGoods.getSubDishesForServing().click();
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeGoods().click();
+        sleep(1);
+        moyoPageCategoryHomeGoods.getSubPlumbing().click();
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeGoods().click();
+        sleep(1);
+        moyoPageCategoryHomeGoods.getSubLighting().click();
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeGoods().click();
+        sleep(1);
+        moyoPageCategoryHomeGoods.getSubSecurityAndAlarm().click();
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeGoods().click();
+        sleep(1);
+        moyoPageCategoryHomeGoods.getSubAlternativeEnergy().click();
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeGoods().click();
+        sleep(1);
+        moyoPageCategoryHomeGoods.getSubHomeDecor().click();
+        moyoHomePage.getMenuBtn().click();
+        moyoHomePage.getMenuCategoryHomeGoods().click();
+        sleep(1);
+        moyoPageCategoryHomeGoods.getSubBagsAndAccessories().click();
+        sleep(1);
+        moyoHomePage.getLogoBtn().click();
+    }
+}
