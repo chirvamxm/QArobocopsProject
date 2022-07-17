b17e261e484860861bc0ab42c27e005b73c12b04
Roman Dovhalenko <romandovhalenko@gmail.com>
2022-07-17 17:48:09 +0300

    QAR-124_AddTestSubMenuAcsessor

--- src/test/java/PageObject/Moyo/ua/MoyoPageCategoryAcsessor.java
+++ src/test/java/PageObject/Moyo/ua/MoyoPageCategoryAcsessor.java
@@ -12,4 +12,52 @@ public class MoyoPageCategoryAcsessor extends BasePage {
     public WebElement getTopGoodsFirst() {
         return getElementsByXpath("//button[@class='buy-btn btn btn--green js-product-modal-close js-buy']").get(0);
     }
+
+    public WebElement getSubAccessoriesForPC() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(0);
+    }
+
+    public WebElement getSubAccessoriesForPhones() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(1);
+    }
+
+    public WebElement getSubAccessoriesForTV() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(2);
+    }
+
+    public WebElement getSubAccessoriesForPhotoVideoAudio() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(3);
+    }
+
+    public WebElement getSubAccessoriesForPortableEquipment() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(4);
+    }
+
+    public WebElement getSubElementsOfPower() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(5);
+    }
+
+    public WebElement getSubChargers() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(6);
+    }
+
+    public WebElement getSubCables() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(7);
+    }
+
+    public WebElement getSubExternalBatteries() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(8);
+    }
+
+    public WebElement getSubHeadphone() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(9);
+    }
+
+    public WebElement getSubMeansOfCare() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(10);
+    }
+
+    public WebElement getSubAccessoriesForGraphicsTablets() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(11);
+    }
 }
