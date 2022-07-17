d17c073511a224a405ad9013ddd545d0b12b9680
Roman Dovhalenko <romandovhalenko@gmail.com>
2022-07-17 17:31:23 +0300

    QAR-121_AddTestSubMenuPhotoVideo

--- src/test/java/PageObject/Moyo/ua/MoyoPageCategoryPhotoVideo.java
+++ src/test/java/PageObject/Moyo/ua/MoyoPageCategoryPhotoVideo.java
@@ -12,4 +12,20 @@ public class MoyoPageCategoryPhotoVideo extends BasePage {
     public WebElement getTopGoodsFirst() {
         return getElementsByXpath("//button[@class='buy-btn btn btn--green js-product-modal-close js-buy']").get(0);
     }
+
+    public WebElement getSubPhotoEquipment() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(0);
+    }
+
+    public WebElement getSubTV() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(1);
+    }
+
+    public WebElement getSubVideoEquipment() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(2);
+    }
+
+    public WebElement getSubAudio() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(3);
+    }
 }
