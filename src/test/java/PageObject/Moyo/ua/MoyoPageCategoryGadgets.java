7eb32cb04884b2e893b6bc93ee030dea0d0490c5
Roman Dovhalenko <romandovhalenko@gmail.com>
2022-07-17 17:23:02 +0300

    QAR-119_AddTestSubMenuGadgets

--- src/test/java/PageObject/Moyo/ua/MoyoPageCategoryGadgets.java
+++ src/test/java/PageObject/Moyo/ua/MoyoPageCategoryGadgets.java
@@ -12,4 +12,44 @@ public class MoyoPageCategoryGadgets extends BasePage {
     public WebElement getTopGoodsFirst() {
         return getElementsByXpath("//button[@class='buy-btn btn btn--green js-product-modal-close js-buy']").get(0);
     }
+
+    public WebElement getSubSmartWatch() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(0);
+    }
+
+    public WebElement getSubFitnessBracelets() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(1);
+    }
+
+    public WebElement getSubPhoneStraps() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(2);
+    }
+
+    public WebElement getSubAircraft() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(3);
+    }
+
+    public WebElement getSubRobots() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(4);
+    }
+
+    public WebElement getSubSmartHome() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(5);
+    }
+
+    public WebElement getSubManualStedykams() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(6);
+    }
+
+    public WebElement getSubManual3DPrint() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(7);
+    }
+
+    public WebElement getSubElectricTransport() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(8);
+    }
+
+    public WebElement getSubVRGlasses() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(9);
+    }
 }
