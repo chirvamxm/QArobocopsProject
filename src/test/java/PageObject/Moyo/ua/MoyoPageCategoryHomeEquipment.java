c47a94a9bfe72d23fe0caba4eefdd7fd4a7efa4e
Roman Dovhalenko <romandovhalenko@gmail.com>
2022-07-17 17:43:13 +0300

    QAR-123_AddTestSubMenuHomeEquipment

--- src/test/java/PageObject/Moyo/ua/MoyoPageCategoryHomeEquipment.java
+++ src/test/java/PageObject/Moyo/ua/MoyoPageCategoryHomeEquipment.java
@@ -12,4 +12,36 @@ public class MoyoPageCategoryHomeEquipment extends BasePage {
     public WebElement getTopGoodsFirst() {
         return getElementsByXpath("//button[@class='buy-btn btn btn--green js-product-modal-close js-buy']").get(0);
     }
+
+    public WebElement getSubBigHomeAppliances() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(0);
+    }
+
+    public WebElement getSubGoodsForTheKitchen() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(1);
+    }
+
+    public WebElement getSubClimateTechnology() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(2);
+    }
+
+    public WebElement getSubEmbeddedTechnology() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(3);
+    }
+
+    public WebElement getSubEquipmentForPersonalUse() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(4);
+    }
+
+    public WebElement getSubSmallHouseholdAppliances() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(5);
+    }
+
+    public WebElement getSubWaterPurification() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(6);
+    }
+
+    public WebElement getSubMedicalEquipment() {
+        return getElementsByXpath("//div[@class='portal-category-list__item portal-category-list__item-hover']").get(7);
+    }
 }
