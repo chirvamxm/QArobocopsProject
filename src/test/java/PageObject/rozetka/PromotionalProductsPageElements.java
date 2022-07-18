132ae0402d88369915df6f43ae18cbade6ee95f4
OleksandrTaran1 <taranoleksandr1992@gmail.com>
2022-07-18 17:04:18 +0300

    test filter promo products

--- src/test/java/PageObject/rozetka/PromotionalProductsPageElements.java
+++ src/test/java/PageObject/rozetka/PromotionalProductsPageElements.java
@@ -12,4 +12,12 @@ public class PromotionalProductsPageElements extends BasePage {
     public WebElement promotionalProducts() {
         return getElementByXpath("//img[@class='promo-tile__picture']");
     }
+
+    public WebElement filterWithDiscounts() {
+        return getElementByXpath("//a[@class='checkbox-filter__link'] [contains(text(), 'Зі знижками')]");
+    }
+
+    public WebElement filterHouseholdGoods() {
+        return getElementByXpath("//a[@class='checkbox-filter__link'] [contains(text(), 'Товари для дому')]");
+    }
 }
