35858501ca412883fef5308566a669269dd58030
Max <chirvamaxym@gmail.com>
2022-07-20 16:21:33 +0300

    Change webElements page

--- src/test/java/PageObject/glovo/HomePageWebElements.java
+++ src/test/java/PageObject/glovo/HomePageWebElements.java
@@ -10,11 +10,18 @@ public class HomePageWebElements extends BasePage {
         super(driver);
     }
 
-
     // HEADER
-
     public static final String GET_STARTED_BTN = "//button[@id='user-register']";
     public static final String PROFILE_BTN = "//img[@alt='Profile']";
+    //SIGN IN WINDOW
+    public static final String LOG_IN_BTN = "//button[@data-e2e-id='header-user-login']";
+    public static final String EMAIL_ADDRESS_FIELD = "//input[@data-test-id='text-field-input'][@name='email']";
+    public static final String PASSWORD_FIELD = "//input[@data-test-id='text-field-input'][@name='password']";
+    public static final String LOG_IN_WITH_EMAIL_BTN = "//button[@id='login-button']";
+    // ADDRESS DELIVERY WINDOW
+    public static final String USE_CURRENT_LOCATION_BTN = "//div[@data-test-id='use-current-location'][@class='current-location']";
+    //FOOTER
+    public static final String SOCIAL_NETWORK_BTN = "//a[@href='%s']";
 
     public WebElement getStartedBtn() {
         return getElementByXpath(GET_STARTED_BTN);
@@ -23,14 +30,6 @@ public class HomePageWebElements extends BasePage {
     public WebElement getProfileBtn() {
         return getElementByXpath(PROFILE_BTN);
     }
-
-
-    //SIGN IN WINDOW
-
-    public static final String LOG_IN_BTN = "//button[@data-e2e-id='header-user-login']";
-    public static final String EMAIL_ADDRESS_FIELD = "//input[@data-test-id='text-field-input'][@name='email']";
-    public static final String PASSWORD_FIELD = "//input[@data-test-id='text-field-input'][@name='password']";
-    public static final String LOG_IN_WITH_EMAIL_BTN = "//button[@id='login-button']";
 
     public WebElement getLogIn() {
         return getElementByXpath(LOG_IN_BTN);
@@ -48,19 +47,9 @@ public class HomePageWebElements extends BasePage {
         return getElementByXpath(LOG_IN_WITH_EMAIL_BTN);
     }
 
-
-    // ADDRESS DELIVERY WINDOW
-
-    public static final String USE_CURRENT_LOCATION_BTN = "//div[@data-test-id='use-current-location'][@class='current-location']";
-
     public WebElement getUseCurrentLocationBtn() {
         return getElementByXpath(USE_CURRENT_LOCATION_BTN);
     }
-
-
-    //FOOTER
-
-    public static final String SOCIAL_NETWORK_BTN = "//a[@href='%s']";
 
     public WebElement getSocialNetworkBtn(String currentNetwork) {
         return getElementByXpath(String.format(SOCIAL_NETWORK_BTN, currentNetwork));
