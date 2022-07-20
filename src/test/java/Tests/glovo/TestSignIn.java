edafee1a9741eb18d5348bd16d72a5ee3e56ff60
Max <chirvamaxym@gmail.com>
2022-07-20 16:07:33 +0300

    QAR-150 Test SignIn

---
+++ src/test/java/Tests/glovo/TestSignIn.java
@@ -0,0 +1,24 @@
+package Tests.glovo;
+
+import PageObject.glovo.HomePageWebElements;
+import Tests.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSignIn extends TestInit {
+
+    @Test
+    public void testSignIn() {
+
+        HomePageWebElements homePageWebElements = new HomePageWebElements(driver);
+
+        goToSite("https://glovoapp.com/ua/uk/");
+        homePageWebElements.getStartedBtn().click();
+        homePageWebElements.getLogIn().click();
+        homePageWebElements.getEmailAddressField().sendKeys("chirvamax@ukr.net");
+        homePageWebElements.getPasswordField().sendKeys("qwerty12345");
+        homePageWebElements.getLogInWithEmailBtn().click();
+        homePageWebElements.getUseCurrentLocationBtn().click();
+        Assert.assertTrue(homePageWebElements.getProfileBtn().isDisplayed());
+    }
+}
